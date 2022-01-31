87e4efb846e2929a4e1b0663db12e49b4fa17f24
Jingying He <450883724@qq.com>
2022-01-31 12:24:01 +1100

    print query

--- create_db.py
+++ create_db.py
@@ -2,96 +2,96 @@ import json
 import sqlite3
 import datetime
 
+def create_db():
+    conn = sqlite3.connect('freshdesk_db.sqlite')
+    cur = conn.cursor()
 
-conn = sqlite3.connect('freshdesk_db.sqlite')
-cur = conn.cursor()
+    # Create table Agents, Tickets, Activity, Act_Order, Act_Note
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Agents(
+            agent_id INTEGER,
+            agents_type CHAR(8),
+            PRIMARY KEY (agent_id)
+        )
+    ''')
 
-# Create table Agents, Tickets, Activity, Act_Order, Act_Note
-cur.execute('''
-    CREATE TABLE IF NOT EXISTS Agents(
-        agent_id INTEGER,
-        agents_type CHAR(8),
-        PRIMARY KEY (agent_id)
-    )
-''')
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Tickets(
+            ticket_id INTEGER,
+            agent_id INTEGER,
+            PRIMARY KEY (ticket_id),
+            FOREIGN KEY (agent_id) REFERENCES Agents (agent_id)
+        )
+    ''')
 
-cur.execute('''
-    CREATE TABLE IF NOT EXISTS Tickets(
-        ticket_id INTEGER,
-        agent_id INTEGER,
-        PRIMARY KEY (ticket_id),
-        FOREIGN KEY (agent_id) REFERENCES Agents (agent_id)
-    )
-''')
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Activity(
+            activity_id INTEGER,
+            ticket_id INTEGER,
+            performed_at DATETIME,
+            PRIMARY KEY (activity_id),
+            FOREIGN KEY (ticket_id) REFERENCES Tickets (ticket_id) ON DELETE CASCADE
+        )
+    ''')
 
-cur.execute('''
-    CREATE TABLE IF NOT EXISTS Activity(
-        activity_id INTEGER,
-        ticket_id INTEGER,
-        performed_at DATETIME,
-        PRIMARY KEY (activity_id),
-        FOREIGN KEY (ticket_id) REFERENCES Tickets (ticket_id) ON DELETE CASCADE
-    )
-''')
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Act_Order(
+            order_id INTEGER,
+            activity_id INTEGER,
+            shipping_address TEXT,
+            shipment_date DATE,
+            priority INTEGER,
+            issue_type CHAR(20),
+            order_status TEXT,
+            order_group CHAR(20),
+            category CHAR(20),
+            order_source INTEGER,
+            requester INTEGER,
+            product CHAR(20),
+            contacted_customer BOOLEAN,
+            PRIMARY KEY (order_id),
+            FOREIGN KEY (activity_id) REFERENCES Activity (activity_id) ON DELETE CASCADE
+        )
+    ''')
 
-cur.execute('''
-    CREATE TABLE IF NOT EXISTS Act_Order(
-        order_id INTEGER,
-        activity_id INTEGER,
-        shipping_address TEXT,
-        shipment_date DATE,
-        priority INTEGER,
-        issue_type CHAR(20),
-        order_status TEXT,
-        order_group CHAR(20),
-        category CHAR(20),
-        order_source INTEGER,
-        requester INTEGER,
-        product CHAR(20),
-        contacted_customer BOOLEAN,
-        PRIMARY KEY (order_id),
-        FOREIGN KEY (activity_id) REFERENCES Activity (activity_id) ON DELETE CASCADE
-    )
-''')
-
-cur.execute('''
-    CREATE TABLE IF NOT EXISTS Act_Note(
-        note_id INTEGER,
-        activity_id INTEGER,
-        note_type INTEGER,
-        PRIMARY KEY (note_id),
-        FOREIGN KEY (note_id) REFERENCES Activity (activity_id) ON DELETE CASCADE
-    )
-''')
-
-
-# Insert JSON data into databse
-all_data = json.load(open('activities.json'))
-activities = all_data['activities_data']
-
-query1 = 'INSERT OR REPLACE INTO Agents VALUES (?,?)'
-query2 = 'INSERT OR REPLACE INTO Tickets VALUES (?,?)'
-query3 = 'INSERT INTO Activity (ticket_id, performed_at) VALUES (?,?)'
-query4 = 'INSERT INTO Act_Order (activity_id,shipping_address, shipment_date,priority,issue_type,order_status,order_group,category,order_source,requester,product,contacted_customer) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)'
-query5 = 'INSERT INTO Act_Note VALUES (?,?,?)'
-
-try:
-    for activity in activities:
-        cur.execute(
-            query1, (activity['performer_id'], activity['performer_type']))
-        cur.execute(query2, (activity['ticket_id'], activity['performer_id']))
-        cur.execute(
-            query3, (activity['ticket_id'], datetime.datetime.strptime(activity['performed_at'], '%d-%m-%Y %H:%M:%S %z')))
-        activity_id = cur.lastrowid
-        if 'note' not in activity['activity'].keys():
-            cur.execute(query4, (activity_id, activity['activity']['shipping_address'], activity['activity']['shipment_date'], activity['activity']['priority'], activity['activity']['issue_type'], activity['activity']['status'],
-                                 activity['activity']['group'], activity['activity']['category'], activity['activity']['source'], activity['activity']['requester'], activity['activity']['product'], activity['activity']['contacted_customer']))
-        else:
-            cur.execute(query5, (activity['activity']['note']['id'],
-                        activity_id, activity['activity']['note']['type']))
-except sqlite3.IntegrityError as e:
-    print('Error occurred: ', e)
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Act_Note(
+            note_id INTEGER,
+            activity_id INTEGER,
+            note_type INTEGER,
+            PRIMARY KEY (note_id),
+            FOREIGN KEY (note_id) REFERENCES Activity (activity_id) ON DELETE CASCADE
+        )
+    ''')
 
 
-cur.close()
-conn.commit()
+    # Insert JSON data into databse
+    all_data = json.load(open('activities.json'))
+    activities = all_data['activities_data']
+
+    query1 = 'INSERT OR REPLACE INTO Agents VALUES (?,?)'
+    query2 = 'INSERT OR REPLACE INTO Tickets VALUES (?,?)'
+    query3 = 'INSERT INTO Activity (ticket_id, performed_at) VALUES (?,?)'
+    query4 = 'INSERT INTO Act_Order (activity_id,shipping_address, shipment_date,priority,issue_type,order_status,order_group,category,order_source,requester,product,contacted_customer) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)'
+    query5 = 'INSERT INTO Act_Note VALUES (?,?,?)'
+
+    try:
+        for activity in activities:
+            cur.execute(
+                query1, (activity['performer_id'], activity['performer_type']))
+            cur.execute(query2, (activity['ticket_id'], activity['performer_id']))
+            cur.execute(
+                query3, (activity['ticket_id'], datetime.datetime.strptime(activity['performed_at'], '%d-%m-%Y %H:%M:%S %z')))
+            activity_id = cur.lastrowid
+            if 'note' not in activity['activity'].keys():
+                cur.execute(query4, (activity_id, activity['activity']['shipping_address'], activity['activity']['shipment_date'], activity['activity']['priority'], activity['activity']['issue_type'], activity['activity']['status'],
+                                    activity['activity']['group'], activity['activity']['category'], activity['activity']['source'], activity['activity']['requester'], activity['activity']['product'], activity['activity']['contacted_customer']))
+            else:
+                cur.execute(query5, (activity['activity']['note']['id'],
+                            activity_id, activity['activity']['note']['type']))
+    except sqlite3.IntegrityError as e:
+        print('Error occurred: ', e)
+
+
+    cur.close()
+    conn.commit()
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 import json
 
 from mock import Mock
+from create_db import create_db
+from query_ticket_lifecycle import query_ticket_lifecycle
 
 
 def custom_command_line():
@@ -17,11 +19,16 @@ def custom_command_line():
         print("Wrong command line format.\nThe right format should be \'ticket_gen -n (numbers) -o (JSON file name)\' ")
 
 
-if __name__ == '__main__':
+def export_JSON():
     tickets_num, filename = custom_command_line()
-
     data = Mock(tickets_num)
     data_dic = data.generate_data()
 
     with open(filename, 'w') as outfile:
         json_object = json.dump(data_dic, outfile)
+
+
+if __name__ == '__main__':
+    export_JSON()
+    create_db()
+    query_ticket_lifecycle()
