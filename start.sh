d7e3fc15974b9bbb5e0382a196ccf96f35fe5c15
Jingying He <450883724@qq.com>
2022-01-31 14:24:14 +1100

    update path in bash scripts

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
-export PYTHONPATH=/opt/anaconda3/envs/aginic_proj/bin/python
+# export PYTHONPATH=/opt/anaconda3/envs/aginic_proj/bin/python
+export PYTHONPATH=aginic_proj/bin/python
 python3 main.py ticket_gen -n 1000 -o activities.json
